72894a21075e6cf7f78f4c158db5c677a0a7839f
kylecustodio <kkzycg@gmail.com>
2025-02-04 19:21:28 -0600

    Initial commit

---
+++ src/main/java/work/custodio/pekin/PekinApplication.java
@@ -0,0 +1,13 @@
+package work.custodio.pekin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PekinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PekinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/custodio/pekin/PekinApplicationTests.java
@@ -0,0 +1,13 @@
+package work.custodio.pekin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PekinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

39cf51fd0344a57ad8cd39177351aa1854d46d3a
kylecustodio <kkzycg@gmail.com>
2025-02-05 23:45:13 -0600

    Add date to transaction

--- src/main/java/work/custodio/pekin/model/Transaction.java
+++ src/main/java/work/custodio/pekin/model/Transaction.java
@@ -3,6 +3,7 @@ package work.custodio.pekin.model;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "transaction")
@@ -16,6 +17,8 @@ public class Transaction {
     private Account account;
 
     private BigDecimal amount;
+
+    private LocalDate date;
 
     public Account getAccount() {
         return account;
@@ -39,5 +42,13 @@ public class Transaction {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 }
